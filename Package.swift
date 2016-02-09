e46bef27f62bfe61b2bbe7a1617fceb718069c3c
Elliott Minns <elliott.minns@me.com>
2016-02-09 16:56:50 +0000

    Using local copy

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "BlackfishStencil",
     dependencies: [
         .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0),
-        .Package(url: "https://github.com/elliottminns/blackfish.git", majorVersion: 0)
+        .Package(url: "/Users/Elliott/Dropbox/Projects/Open Source/blackfish/", majorVersion: 0)
     ]
 )
