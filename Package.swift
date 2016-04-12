08d3740dc54711a7cdf15d0342c4fbc1e397af87
Elliott Minns <elliott.minns@me.com>
2016-04-12 19:19:51 +0100

    Updated for swift 3.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "BlackfishStencil",
     dependencies: [
-        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0),
+        .Package(url: "https://github.com/ratranqu/Stencil.git", majorVersion: 0),
         .Package(url: "https://github.com/elliottminns/blackfish.git", majorVersion: 0)
     ]
 )
---
+++ Xcode/BlackfishStencil/BlackfishStencil/BlackfishStencil.h
@@ -0,0 +1,19 @@
+//
+//  BlackfishStencil.h
+//  BlackfishStencil
+//
+//  Created by Elliott Minns on 10/02/2016.
+//  Copyright © 2016 Elliott Minns. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BlackfishStencil.
+FOUNDATION_EXPORT double BlackfishStencilVersionNumber;
+
+//! Project version string for BlackfishStencil.
+FOUNDATION_EXPORT const unsigned char BlackfishStencilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlackfishStencil/PublicHeader.h>
+
+
